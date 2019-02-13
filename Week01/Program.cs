05e95fa543e68a4214dd1e244dee919a10a7daec
User <User@DEV-022774.OnBase.net>
2019-02-12 19:22:16 -0500

    Added a profiles

--- Week01/Program.cs
+++ Week01/Program.cs
@@ -7,7 +7,21 @@ namespace Week01
         static void Main(string[] args)
         {
             // say Hello World here
-            
+            Console.WriteLine("Hello World!");
+            string Name = ("Dwight");
+            int Age = 34;
+            string favColor = ("Brown and yellow");
+            string Hobbie = ("Farming and karate");
+            bool canYoudrive = true;
+            double height = 6.3;
+            char Initial = 'D';
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("My age is " + Age);
+            Console.WriteLine("My fav color is " + favColor);
+            Console.WriteLine("My fav hobbie is " + Hobbie);
+            Console.WriteLine("Can I drive " + canYoudrive);
+            Console.WriteLine("My hight is " + height + "feet tall");
+            Console.WriteLine("My first initial is " + Initial);
         }
     }
 }
